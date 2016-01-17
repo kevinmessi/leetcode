1a1e20a0968e88a8e135e4eb52fa500bdb9b8f18
kevinrjt <kevinrjt@163.com>
2016-01-17 21:48:08 +0800

    backup

---
+++ WordBreak/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+bool wordBreak(string s, unordered_set<string>& wordDict)
+{
+    return true;
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
