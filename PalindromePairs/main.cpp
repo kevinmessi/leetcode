f5a49fc7ce854a80d7a28ed58010aa1d084061c7
kevinrjt <kevinrjt@163.com>
2016-06-08 23:21:51 +0800

    backup

--- PalindromePairs/main.cpp
+++ PalindromePairs/main.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+//Given a list of unique words.
+//Find all pairs of distinct indices (i, j) in the given list,
+//so that the concatenation of the two words,
+//i.e. words[i] + words[j] is a palindrome.
 vector<vector<int>> palindromePairs(vector<string> &words)
 {
     vector<vector<int>> pairs;
@@ -13,7 +17,17 @@ vector<vector<int>> palindromePairs(vector<string> &words)
 
 int main()
 {
+//    Example 1:
+//    Given words = ["bat", "tab", "cat"]
+//    Return [[0, 1], [1, 0]]
+//    The palindromes are ["battab", "tabbat"]
+//    Example 2:
+//    Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+//    Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+//    The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
     vector<string> words{ "bat", "tab", "cat" };
+    PrintMatrix(palindromePairs(words));
+    words = { "abcd", "dcba", "lls", "s", "sssll" };
     PrintMatrix(palindromePairs(words));
     return 0;
 }
--- ShortestPalindrome/main.cpp
+++ ShortestPalindrome/main.cpp
@@ -1,14 +1,76 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+string shortestPalindrome1(string s)
+{
+    int len = s.length();
+    string p("#");
+    for (char c : s)
+        p.append(1, c).append(1, '#');
+    const char *t = p.c_str();
+    vector<int> max_len(len + 1);
+    max_len[0] = 1;
+    for (int i = 1, mi = 0; i <= len; ++i)
+    {
+        int ri = mi + max_len[mi];
+        max_len[i] = ri > i ? min(max_len[2 * mi - i], ri - i) : 1;
+        while (i >= max_len[i] && t[i - max_len[i]] == t[i + max_len[i]])
+            ++max_len[i];
+        if (i + max_len[i] > ri) mi = i;
+    }
+    for (int i = len; i >= 0; --i)
+    {
+        if (max_len[i] == i + 1)
+        {
+            string r = s.substr(i);
+            reverse(r.begin(), r.end());
+            return r + s;
+        }
+    }
+    return "";
+}
+
+string shortestPalindrome2(string s)
+{
+    string r(s);
+    reverse(r.begin(), r.end());
+    string p = s + '#' + r;
+    vector<int> next(p.length(), 0);
+    for (size_t i = 1, k = 0; i < p.length(); ++i)
+    {
+        while (k > 0 && p[k] != p[i])
+            k = next[k - 1];
+        if (p[k] == p[i]) ++k;
+        next[i] = k;
+    }
+    return r.substr(0, r.length() - next.back()) + s;
+}
+
 string shortestPalindrome(string s)
 {
-    return s;
+    if (s.empty()) return "";
+    string r(s);
+    reverse(r.begin(), r.end());
+    string p = s + r;
+    vector<int> next(p.length(), 0);
+    for (size_t i = 1, k = 0; i < p.length(); ++i)
+    {
+        while (k > 0 && p[k] != p[i])
+            k = next[k - 1];
+        if (p[k] == p[i]) ++k;
+        next[i] = k;
+    }
+    size_t x = next.back();
+    while (x > s.length())
+        x = next[x - 1];
+    return r.substr(0, r.length() - x) + s;
 }
 
 int main()
 {
-    cout << shortestPalindrome("Hello World!") << endl;
+    cout << shortestPalindrome("abccbabc") << endl;
     return 0;
 }
